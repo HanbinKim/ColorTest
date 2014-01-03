45c974dd17fe43127b80cefde0c127bb2bd702f9
HanbinKim <hottuc@naver.com>
2014-01-03 16:17:17 +0900

    2.10.2 실습 칼라 슬라이더 완료했습니다.
    슬라이드를 통해서 뷰색상을 변경합니다.

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,11 +9,54 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
 @implementation ViewController
+@synthesize redSlider;
+@synthesize greenSlider;
+@synthesize blueSlider;
+@synthesize colorView;
+@synthesize activityIndicator;
 
+-(IBAction)onColorSliderChanged:(id)sender
+{
+    if([activityIndicator isAnimating])
+    {
+        [activityIndicator stopAnimating];
+    }
+    colorView.backgroundColor = [UIColor colorWithRed:redSlider.value green:greenSlider.value
+ blue:blueSlider.value alpha:1.0];
+}
+-(IBAction)powerOnOff:(id)sender
+{
+    BOOL isOn = ((UISwitch*)sender).on;
+    
+    redSlider.enabled =isOn;
+    greenSlider.enabled =isOn;
+    blueSlider.enabled = isOn;
+    
+    activityIndicator.hidesWhenStopped=YES;
+    
+    if(isOn)
+    {
+        [activityIndicator startAnimating];
+        [self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0];
+    }
+    else{
+        colorView.backgroundColor=[UIColor grayColor];
+    }
+    
+}
+-(void) viewDidAppear:(BOOL)animated
+{
+    [self onColorSliderChanged:nil];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
